5d2d765ad3bd7e3f3a1a7a0b448c5ab4d2f13fec
Alejandro Gramajo <agramajo@gmail.com>
2016-09-29 13:40:22 -0300

    Fix

--- public/app.js
+++ public/app.js
@@ -40,7 +40,7 @@ app.run(function($rootScope, $http, $location, $window) {
 app.controller('Home', function($window) {
     var vm = this;
 
-    var decoded = parseJwt($window.sessionStorage.token);
+    var decoded = getUserFromJwt($window.sessionStorage.token);
 
     console.log(decoded);
 
@@ -71,10 +71,10 @@ app.controller('Login', function($location, $http, $window) {
     };
 });
 
-function parseJwt(token) {
+function getUserFromJwt(token) {
 	var base64Url = token.split('.')[1];
     var base64 = base64Url.replace('-', '+').replace('_', '/');
-    return JSON.parse(window.atob(base64));
+    return window.atob(base64);
 };
 
 /*
